32a3dac02394eddf43259713f94a4578ee19aa87
Avni Goel <avnigoel98@gmail.com>
2023-11-20 23:38:19 +0530

    completed lists

---
+++ session9/prog1.py
@@ -0,0 +1,80 @@
+# numbers = [10, 20, 30, 40, 50]
+
+
+
+# length = len(numbers)
+
+# for i in range(length):
+#     print(numbers[i] , -(length - i))
+
+
+# spam = ['cat', 'bat', 'rat', 'elephant']
+
+# string = "cat"
+
+# for j, i in enumerate(string):
+#     print(j , i)
+
+
+
+numbers = [1, 20, 3, 4, 5 , 20, 20]
+
+# num_to_check = 3
+
+# if num_to_check not in numbers:
+#     print("not present")
+# else:
+#     print("yes present")
+
+
+# import random
+
+# pets = ['Dog', 'Cat', 'Moose']
+
+# print(random.choice(pets))
+
+
+# random.shuffle(pets)
+
+# print(pets)
+
+
+
+# a = 2
+# print(a) # 2
+# a += 1 # a = a + 1
+# print(a) # 3
+# a -= 3  # a = a - 3
+# print(a) # 0
+# a *= 2
+# print(a) # 0
+# a /= 2 
+# print(a) # 0
+# a %= 2
+# print(0)
+
+
+spam = "2"
+spam += ' world!'
+
+print(spam)
+
+
+number = 2
+square = number ** 2
+cube = number ** 3
+print(square)
+print(cube)
+
+
+spam = [2, 5, 3.14, 1, -7]
+spam.sort()
+spam.reverse()
+print(spam)
+
+
+
+# mutable - lists (add, update, remove)
+# immutable - tuple 
+list = []
+tuple = (3 , 4, 57, 8)
