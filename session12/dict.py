cb17299953bb131b669e2f89bd431b8fe6797772
Avni Goel <avnigoel98@gmail.com>
2023-12-17 09:51:29 +0530

    working on dictionary

---
+++ session12/dict.py
@@ -0,0 +1,35 @@
+# dictionary is a collection of key-value pair
+# list = []
+# tuple = ()
+
+
+mydict = {
+    "fast" : "in a quick manner",
+    "Aditi" : "a friend",
+     5: 7,
+     7.8 : "a number",
+     "Aditi" : "a dancer",
+     "avantika" : "a friend",
+     "marks": [1, 4, 8],
+     "anotherDict" : {"Aditi" : "a coder"},
+    
+}
+
+#print(dict)
+# print(dict["fast"])
+# print(dict["anotherDict"]["harry"])
+# print(dict["marks"][0])
+
+#print(mydict.keys())
+
+# print(type(dict.keys()))
+# print(dict.values())
+
+# print(mydict.items())
+# print(mydict)
+
+# #print(dict["color"])
+# print(dict.get("color" , "hello"))
+# print(dict.get("Aditi", "hi"))
+
+dict = {}
